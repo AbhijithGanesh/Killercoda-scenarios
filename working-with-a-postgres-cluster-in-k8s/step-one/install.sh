d5060353bbea9b35733ef4c85dc16d9d13ade3f0
Abhijith Ganesh <67182544+AbhijithGanesh@users.noreply.github.com>
2022-07-22 23:08:44 +0530

    Addition of cluster YAML
    Signed-off-by: Abhijith Ganesh <67182544+AbhijithGanesh@users.noreply.github.com>

--- working-with-a-postgres-cluster-in-k8s/step-one/install.sh
+++ working-with-a-postgres-cluster-in-k8s/step-one/install.sh
@@ -11,5 +11,21 @@ kubectl apply -f https://raw.githubusercontent.com/rancher/local-path-provisione
 kubectl patch storageclass local-path -p '{"metadata": {"annotations":{"storageclass.kubernetes.io/is-default-class":"true"}}}'
 kubectl apply -f https://raw.githubusercontent.com/cloudnative-pg/cloudnative-pg/release-1.16/releases/cnpg-1.16.0.yaml
 sleep 10
-kubectl apply -f https://raw.githubusercontent.com/AbhijithGanesh/Killercoda-scenarios/master/working-with-cloudnative-pg/step-two/manifest.yaml
-clear  
+echo > "apiVersion: postgresql.cnpg.io/v1
+kind: Cluster
+metadata:
+  name: cluster-example
+spec:
+  instances: 3
+  # Example of rolling update strategy:
+  # - unsupervised: automated update of the primary once all
+  #                 replicas have been upgraded (default)
+  # - supervised: requires manual supervision to perform
+  #               the switchover of the primary
+  primaryUpdateStrategy: unsupervised
+
+  # Require 1Gi of space
+  storage:
+    size: 500Mi
+">cluster.yaml
+kubectl apply -f cluster.yaml
