31b852982d450d38f0ee578f086a4ce2357d19ae
Abhijith Ganesh <67182544+AbhijithGanesh@users.noreply.github.com>
2022-07-22 22:33:03 +0530

    Addition of second challenge (#2)
    Signed-off-by: Abhijith Ganesh <67182544+AbhijithGanesh@users.noreply.github.com>

---
+++ working-with-a-postgres-cluster-in-k8s/step-one/install.sh
@@ -0,0 +1,12 @@
+#!bin/bash
+
+# Author: Abhijith Ganesh
+# Scenario: Working of Kubernetes
+
+
+curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
+chmod 700 get_helm.sh
+./get_helm.sh
+kubectl apply -f https://raw.githubusercontent.com/rancher/local-path-provisioner/master/deploy/local-path-storage.yaml
+kubectl patch storageclass local-path -p '{"metadata": {"annotations":{"storageclass.kubernetes.io/is-default-class":"true"}}}'
+kubectl apply -f https://raw.githubusercontent.com/cloudnative-pg/cloudnative-pg/release-1.16/releases/cnpg-1.16.0.yaml
