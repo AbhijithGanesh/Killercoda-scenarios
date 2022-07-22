0897b407d1f6c8a5898eb311a9f354985fc3d4a6
Abhijith Ganesh <67182544+AbhijithGanesh@users.noreply.github.com>
2022-07-22 22:40:29 +0530

    Manifest update🔱
    Signed-off-by: Abhijith Ganesh <67182544+AbhijithGanesh@users.noreply.github.com>

--- working-with-a-postgres-cluster-in-k8s/step-one/install.sh
+++ working-with-a-postgres-cluster-in-k8s/step-one/install.sh
@@ -10,3 +10,4 @@ chmod 700 get_helm.sh
 kubectl apply -f https://raw.githubusercontent.com/rancher/local-path-provisioner/master/deploy/local-path-storage.yaml
 kubectl patch storageclass local-path -p '{"metadata": {"annotations":{"storageclass.kubernetes.io/is-default-class":"true"}}}'
 kubectl apply -f https://raw.githubusercontent.com/cloudnative-pg/cloudnative-pg/release-1.16/releases/cnpg-1.16.0.yaml
+kubectl apply -f https://raw.githubusercontent.com/AbhijithGanesh/Killercoda-scenarios/master/working-with-cloudnative-pg/step-two/manifest.yaml
