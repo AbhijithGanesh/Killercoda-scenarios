1058096b70e11f85767152b49a33d489337ee156
Abhijith Ganesh <67182544+AbhijithGanesh@users.noreply.github.com>
2022-08-03 21:32:54 +0530

    Capacity update
    Signed-off-by: Abhijith Ganesh <67182544+AbhijithGanesh@users.noreply.github.com>

--- Lets-install-mongo-db/rancher.sh
+++ Lets-install-mongo-db/rancher.sh
@@ -5,6 +5,3 @@
 
 kubectl apply -f https://raw.githubusercontent.com/rancher/local-path-provisioner/master/deploy/local-path-storage.yaml
 kubectl patch storageclass local-path -p '{"metadata": {"annotations":{"storageclass.kubernetes.io/is-default-class":"true"}}}'
-
-helm repo add percona https://percona.github.io/percona-helm-charts/
-helm repo update
