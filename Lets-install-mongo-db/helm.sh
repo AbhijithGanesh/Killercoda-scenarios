299c58c5ab3ce833a2955f66e537035c41c8f617
Abhijith Ganesh <67182544+AbhijithGanesh@users.noreply.github.com>
2022-08-03 21:05:30 +0530

    Upgrades
    Signed-off-by: Abhijith Ganesh <67182544+AbhijithGanesh@users.noreply.github.com>

--- Lets-install-mongo-db/helm.sh
+++ Lets-install-mongo-db/helm.sh
@@ -7,6 +7,6 @@
 curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
 chmod 700 get_helm.sh
 ./get_helm.sh
-kubectl apply --server-side -f https://raw.githubusercontent.com/percona/percona-server-mongodb-operator/v1.12.0/deploy/bundle.yaml
+# kubectl apply --server-side -f https://raw.githubusercontent.com/percona/percona-server-mongodb-operator/v1.12.0/deploy/bundle.yaml
 
 clear
