d6d95382873604ec903a57b208ff43db763d9a17
Abhijith Ganesh <git.watch.abhijith@outlook.com>
2023-07-11 10:03:32 +0530

    Addition of corerect shell file
    Signed-off-by: Abhijith Ganesh <git.watch.abhijith@outlook.com>

--- working-with-mongodb/mongo.sh
+++ working-with-mongodb/mongo.sh
@@ -2,7 +2,7 @@ apt install gnupg
 wget -qO- https://www.mongodb.org/static/pgp/server-6.0.asc | sudo tee /etc/apt/trusted.gpg.d/server-6.0.asc
 sudo apt-get update
 sudo apt-get install -y mongodb-mongosh
-cat 'version: "3.9"
+echo 'version: "3.9"
 services:
    mongo:
       image: mongo
