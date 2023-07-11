3c8bf701dfd0ff25a873df20c60c1823d9c3001c
Abhijith Ganesh <git.watch.abhijith@outlook.com>
2023-07-11 10:48:35 +0530

    You are good to go
    Signed-off-by: Abhijith Ganesh <git.watch.abhijith@outlook.com>

--- working-with-mongodb/mongo.sh
+++ working-with-mongodb/mongo.sh
@@ -1,8 +1,3 @@
-apt install gnupg
-wget -qO- https://www.mongodb.org/static/pgp/server-6.0.asc | sudo tee /etc/apt/trusted.gpg.d/server-6.0.asc
-echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu focal/mongodb-org/6.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-6.0.list
-sudo apt-get update
-sudo apt-get install -y mongodb-mongosh
 echo '
 version: "3"
 services:
