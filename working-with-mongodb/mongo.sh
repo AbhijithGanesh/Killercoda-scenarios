c85cd18e161e6b08ac0683db93ad88f777595116
Abhijith Ganesh <git.watch.abhijith@outlook.com>
2023-07-11 10:14:10 +0530

    Removal of versioning from docker-compose
    Signed-off-by: Abhijith Ganesh <git.watch.abhijith@outlook.com>

--- working-with-mongodb/mongo.sh
+++ working-with-mongodb/mongo.sh
@@ -2,7 +2,7 @@ apt install gnupg
 wget -qO- https://www.mongodb.org/static/pgp/server-6.0.asc | sudo tee /etc/apt/trusted.gpg.d/server-6.0.asc
 sudo apt-get update
 sudo apt-get install -y mongodb-mongosh
-echo 'version: "3.9"
+echo '
 services:
    mongo:
       image: mongo
