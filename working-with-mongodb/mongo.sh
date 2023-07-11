588ec0e91de7ad26be649931a4de21e63b3c3775
Abhijith Ganesh <git.watch.abhijith@outlook.com>
2023-07-11 10:27:08 +0530

    Changes to versioning
    Signed-off-by: Abhijith Ganesh <git.watch.abhijith@outlook.com>

--- working-with-mongodb/mongo.sh
+++ working-with-mongodb/mongo.sh
@@ -1,5 +1,5 @@
 apt install gnupg
-wget -qO- https://www.mongodb.org/static/pgp/server-6.0.asc | sudo tee /etc/apt/trusted.gpg.d/server-6.0.asc
+wget -qO- https://www.mongodb.org/static/pgp/server-6.0.asc  sudo tee /etc/apt/trusted.gpg.d/server-6.0.asc
 sudo apt-get update
 sudo apt-get install -y mongodb-mongosh
 echo '
