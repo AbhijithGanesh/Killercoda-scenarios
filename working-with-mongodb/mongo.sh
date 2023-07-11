84809c17b8ec8aabe633ead89e50e581a836abd7
Abhijith Ganesh <git.watch.abhijith@outlook.com>
2023-07-11 09:31:58 +0530

    Commit update
    Signed-off-by: Abhijith Ganesh <git.watch.abhijith@outlook.com>

--- working-with-mongodb/mongo.sh
+++ working-with-mongodb/mongo.sh
@@ -1,5 +1,9 @@
+wget -qO- https://www.mongodb.org/static/pgp/server-6.0.asc | sudo tee /etc/apt/trusted.gpg.d/server-6.0.asc
+echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu focal/mongodb-org/6.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-6.0.list
 sudo apt-get update -y
 sudo apt-get upgrade -y
 sudo apt install mongodb-org -y
+sudo apt-get install -y mongodb-mongosh
+sudo apt-get install -y mongodb-mongosh-shared-openssl3
 sudo systemctl enable mongod
 sudo systemctl start mongod
