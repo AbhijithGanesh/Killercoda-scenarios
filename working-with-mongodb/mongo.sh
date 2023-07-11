23b9249ceef80716667c80849ecd0b9e6481a7d0
Abhijith Ganesh <git.watch.abhijith@outlook.com>
2023-07-11 15:21:40 +0530

    Post update patch
    Signed-off-by: Abhijith Ganesh <git.watch.abhijith@outlook.com>

--- working-with-mongodb/mongo.sh
+++ working-with-mongodb/mongo.sh
@@ -2,4 +2,12 @@ apt install gnupg
 wget -qO- https://www.mongodb.org/static/pgp/server-6.0.asc | sudo tee /etc/apt/trusted.gpg.d/server-6.0.asc
 sudo apt-get update
 sudo apt-get install -y mongodb-mongosh
+cat 'version: "3.9"
+services:
+   mongo:
+      image: mongo
+      container_name: mongo_db
+      ports:
+         - 27017:27017
+' > docker-compose.yml
 docker-compose up
